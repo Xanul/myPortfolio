58230ee750e7e0ca809267b6633adb9441260be4
Xanul <rivas.060@gmail.com>
2022-05-02 17:28:46 -0500

    Added Skills section with toggle animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,25 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Accordion skills 
+const skillsContent = document.getElementsByClassName('skills__content')
+console.log(skillsContent)
+const skillsHeader = document.querySelectorAll('.skills__header')
+console.log(skillsHeader)
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+    
+    for (i=0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
