448665f911cdb39c53c93e1f64c4d8bec8f9a635
Xanul <rivas.060@gmail.com>
2022-05-02 20:29:48 -0500

    Added Qualification section with animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,9 +33,9 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // Accordion skills 
 const skillsContent = document.getElementsByClassName('skills__content')
-console.log(skillsContent)
+// console.log(skillsContent)
 const skillsHeader = document.querySelectorAll('.skills__header')
-console.log(skillsHeader)
+// console.log(skillsHeader)
 
 function toggleSkills() {
     let itemClass = this.parentNode.className
@@ -50,5 +50,26 @@ function toggleSkills() {
 
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
+})
+
+// Qualification tabs
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=>{
+        
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => { 
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab=>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
 })
 
